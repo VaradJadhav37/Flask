1c2c3ca97cee09c71db127246d01a1406508c64e
Varad Jadhav <142712416+VaradJadhav37@users.noreply.github.com>
2025-04-13 21:48:24 +0530

    jsontest

--- app.py
+++ app.py
@@ -4,5 +4,11 @@ app=Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+@app.route('/jsontest')
+def json_test():
+    return {'name': 'John', 'age': 28, 'city': 'New York',
+            'array of objects': [{'name': 'John', 'age': 29, 'city': 'New York'},
+                                     
+                                    {'name': 'John', 'age': 30, 'city': 'New York'}],}
 if __name__ == '__main__':
     app.run(debug=True)
